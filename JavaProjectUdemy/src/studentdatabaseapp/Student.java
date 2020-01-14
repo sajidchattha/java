e93c9442486a2ec9a67ad4a16c14ef69b5b203b5
sajid <sajid@sajid.com>
2020-01-14 15:10:53 +0100

    enrollment and tution methods implemented

--- JavaProjectUdemy/src/studentdatabaseapp/Student.java
+++ JavaProjectUdemy/src/studentdatabaseapp/Student.java
@@ -8,11 +8,12 @@ public class Student {
 	 private String lastName;
 	 private int gradYear;
 	 private String studentId;
-	 private String courses;
+	 private String courses = "";
 	 private int tutionBalance;
 	 private int costPerCourse = 600;
 	 
 	 private static int id=1001;
+	 private static int i = 1;
 	 
 	 public Student() {
 		 
@@ -28,13 +29,7 @@ public class Student {
 	
 		 
 		 setStudentId();
-		 
-		 
-		 
-		 System.out.println(firstName + " " + lastName + " " + "\nGraduation Year " + gradYear + "\nID: " + studentId );
-		 
-		
-		 
+		 enroll();
 		 
 	 }
 	//Constructor: prompts user to enter student's name and year
@@ -49,11 +44,45 @@ public class Student {
 	 }
 	
 	//Enroll in courses
+	 
+	 public void enroll() {
+		 
+		  
+		do {
+			Scanner c = new Scanner(System.in);
+		
+		 System.out.println("Enter the course to enroll (or press Q to quite): \n");
+		 String course = "";
+		 course = c.nextLine();
+		 
+		if (course.equals("Q")) {
+			 System.out.println("Enrollment Finished");
+			 break;
+			
+			 
+			 } else if(course.equals("")){
+					System.out.println("Fill the field with course name or press Q to quite");}
+			 else{
+				 courses = courses + "Course" + i + ". " + course + " \n";
+				 i++;
+				
+		 }
+		} while (1 != 0 ); 
+		 
+		 
+		
+	 }
 	
 	// View balance 
 	
 	// Pay Tution
 	
 	//Show status
+	 public void Info() {
+		 System.out.println("*****PERSONAL INFORMATION*****\n" + "\nFull Name: " + firstName + " " + lastName + " " + "\nGraduation Year: " + gradYear + "\nID: " + studentId );
+		 System.out.println("\n*****Enrolled Courses***** \n" + "\n" + courses);
+		 System.out.println("Number of enrolled courses: " + (i-1));
+		 System.out.println("Total cost of enrolled courses: "+ (i-1)*costPerCourse + "$");
+	 }
 
 }
--- JavaProjectUdemy/src/studentdatabaseapp/StudentDatabaseApp.java
+++ JavaProjectUdemy/src/studentdatabaseapp/StudentDatabaseApp.java
@@ -1,11 +1,25 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
 		
 		
 		Student stud1 = new Student();
+		//stud1.enroll();
+		
+		System.out.println("PRESS b or B to show information regarding enrollment");
+		String button = "";
+		Scanner bt = new Scanner(System.in);
+		button = bt.nextLine();
+		
+		if (button.equals("b") || bt.equals("B")) {
+			stud1.Info();
+			
+		}
+	//
 		
 		// Ask the user how many new students will be added to the database
 		
